ee076fbe35f3e4ac005638a829cb4677bff54e0a
Weston <w@Office2Arch.localdomain>
2020-09-10 16:55:05 +0100

    new update

---
+++ .config/scripts/wall-set.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#if [ $(xrandr | grep -sw 'connected' | wc -l) -eq 1 ] ;then
+#	if [ $(xrandr | grep -sw '5120x1440' | wc -l) -ge 1 ] ;then
+#		feh --bg-fill /home/w/Documents/wallpapers5120/aus5120scene2.jpg &
+#	elif [ $(xrandr | grep -sw '2560x1440' | wc -l) -ge 1 ] ;then
+#	  feh --bg-fill /home/w/Documents/wallpapers2560/austria-salzburg-city_2560x1440.jpg &
+#	fi
+#else
+# nitrogen --restore &
+#fi
+#
+#
+[ $(xrandr | grep -sw 'connected' | wc -l) = 1 ] && [ $(xrandr | grep -sw '5120x1440' | wc -l) > 1 ] && feh --bg-fill /home/w/Documents/Wallpapers/wallpapers5120/aus5120scene2.jpg 
+[ $(xrandr | grep -sw 'connected' | wc -l) = 1 ] && [ $(xrandr | grep -sw 'connected 2560x1440' | wc -l) = 1 ] && feh --bg-fill /home/w/Documents/Wallpapers/wallpapers2560/Hallstatt_Town_Day_2020_2560x1440.jpg 
+[ $(xrandr | grep -sw 'connected' | wc -l) = 2 ] && feh --bg-center /home/w/Documents/Wallpapers/wallpapers2560/austria-salzburg-city_2560x1440.jpg /home/w/Documents/Wallpapers/wallpapers2560/ws_Hallstatt_Austria_2560x1440.jpg 
