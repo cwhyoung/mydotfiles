07152ef3c4e8c71911fdec59625d1c5aae76fa29
wy1 <wy1>
2021-09-08 18:48:09 +0100

    updating dwmblocks

--- .config/dwmblocks/blocks.def.h
+++ .config/dwmblocks/blocks.def.h
@@ -2,29 +2,30 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"", "/home/w/.local/bin/statusbar/sb-volume",				0,		10},
+	{"", "sb-volume",				0,		10},
 
-	{"", "/home/w/.local/bin/statusbar/sb-pacpackages",				1800,		1},
+	{"", "sb-pacpackages",				1800,		1},
 
-	{"", "/home/w/.local/bin/statusbar/sb-forecast",				1800,		2},
+	{"", "sb-forecast",				1800,		2},
 
-	{"", "/home/w/.local/bin/statusbar/sb-moonphase",				7000,		3},
+	{"", "sb-moonphase",				7000,		3},
 
-	{"", "/home/w/.local/bin/statusbar/sb-internet",				10,		4},
+	{"", "sb-internet",				10,		4},
 
-	{"", "/home/w/.local/bin/statusbar/sb-nettraf",				1,		5},
+	{"", "sb-nettraf",				1,		5},
 
-	{"", "/home/w/.local/bin/statusbar/sb-disk",				50,		6},
+	{"", "sb-disk",				50,		6},
 
-	{"", "/home/w/.local/bin/statusbar/sb-cpu",					1,		7},
+	{"", "sb-cpu",					1,		7},
 
-	{"", "/home/w/.local/bin/statusbar/sb-cpubars",				1,		8},
+	{"", "sb-cpubars",				1,		8},
 
-	{"", "/home/w/.local/bin/statusbar/sb-memory",				2,		9},
+	{"", "sb-memory",				2,		9},
 
-	{"", "/home/w/.local/bin/statusbar/sb-clock",				2,		11},
+	{"", "sb-clock",				2,		11},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
+
